31da107e679744fd9d33f5bc8ed8b09ec7b4d2ff
Steven Morey <moreyse@gmail.com>
2024-10-10 20:51:46 -0400

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+  private final XboxController m_controller = new XboxController(0);
+  private final Drivetrain m_swerve = new Drivetrain();
+  private final Field2d m_field = new Field2d();
+
+
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+
+  @Override
+  public void teleopInit() {
+    // Do this in either robot or subsystem init
+    SmartDashboard.putData("Field", m_field);
+
+  }
+  
+  @Override
+  public void autonomousPeriodic() {
+    driveWithJoystick(false);
+    m_swerve.updateOdometry();
+
+    // Do this in either robot periodic or subsystem periodic
+    m_field.setRobotPose(m_swerve.m_odometry.getPoseMeters());
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    publishToDashboard();
+    driveWithJoystick(false);
+    m_swerve.updateOdometry();
+
+    // Do this in either robot periodic or subsystem periodic
+    m_field.setRobotPose(m_swerve.m_odometry.getPoseMeters());
+
+  }
+
+  public void driveWithJoystick(boolean fieldRelative) {
+    
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    final var xSpeed =
+        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.02))
+            * Drivetrain.kMaxSpeed;
+    SmartDashboard.putNumber("xSpeed", xSpeed);
+
+    // Get the y speed or sideways/strafe speed. We are inverting this because
+    // we want a positive value when we pull to the left. Xbox controllers
+    // return positive values when you pull to the right by default.
+    final var ySpeed =
+        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.02))
+            * Drivetrain.kMaxSpeed;
+    SmartDashboard.putNumber("ySpeed", ySpeed);
+
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    final var rot =
+        -m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02))
+            * Drivetrain.kMaxAngularSpeed;
+    SmartDashboard.putNumber("rot", rot);
+
+    //"A" button sets all the motion to 0
+    if(m_controller.getAButton())
+    {
+      System.out.println("A Pressed");
+      m_swerve.drive(0,0,0,fieldRelative,getPeriod());
+    } else {
+      m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
+    }
+    
+  }
+
+
+
+  public void publishToDashboard()
+  {
+    SmartDashboard.putNumber("Controller Left X", m_controller.getLeftX());
+    SmartDashboard.putNumber("Controller Left Y", m_controller.getLeftY());
+    SmartDashboard.putNumber("Controller Right X", m_controller.getRightX());
+    SmartDashboard.putNumber("Gyro Angle", m_swerve.m_gyro.getAngle());
+  }
+}
